8aa964a52ac97e06d4e02928196a12f30d7b24c1
K2898 <K2898@OHJE-330-WS-27>
2017-02-09 10:46:24 +0200

    fianlly 1 workimnggg

---
+++ Demo05/Persons.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05
+{
+    class Persons
+    {
+        private List<Person> persons;
+
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+        /// <summary>
+        /// Add persons to collection
+        /// </summary>
+        /// <param name="person">person to add</param>
+        public void AddPerson (Person person)
+        {
+            persons.Add(person);
+        }
+
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count) return persons.ElementAt(index);
+            else return null;
+        }
+
+        public Person FindPerson(string SocialSecurityNumber)
+        {
+            foreach(Person person in persons)
+            {
+                // compare two strings
+                if (SocialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person; 
+                }
+            }
+            return null; // person with search socials... not found in collection
+        }
+
+        public void PrintData()
+        {
+            foreach(Person person in persons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
--- Demo05/Program.cs
+++ Demo05/Program.cs
@@ -10,6 +10,51 @@ namespace Demo05
     {
         static void Main(string[] args)
         {
+            // create Persons (collection)
+            Persons myFriends = new Persons();
+
+            //create a few Person objects
+            Person person1 = new Person
+            {
+                Firstname = "Jesse",
+                Lastname = "McCree",
+                SocialSecurityNumber = "010101-123A"
+            };
+            Person person2 = new Person
+            {
+                Firstname = "Tracer",
+                Lastname = "OP",
+                SocialSecurityNumber = "020202-234B"
+            };
+
+            // add person objects t opersons (myfriends)
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+
+            // get one more person
+            Person person4 = myFriends.GetPerson(0);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            } else
+            {
+                Console.WriteLine("Can't find that person");
+            }
+            // tulostetaan collection
+            myFriends.PrintData();
+
+            //etsitään person
+            string sotu = "5224522-2223342";
+            Console.WriteLine("Etsi henkilä jolla sotu: " + sotu);
+            Person person5 = myFriends.FindPerson(sotu);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Kyseistä henkilöä ei löydy valitulla sotulla kek.");
+            }
         }
     }
 }
